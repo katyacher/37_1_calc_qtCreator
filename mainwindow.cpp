53cbabf88c9d5692fffde662a4abaa3719bd0d87
katyacher <katyacher@inbox.ru>
2024-04-04 11:34:05 +0300

    first commit

---
+++ mainwindow.cpp
@@ -0,0 +1,59 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    connect(ui->pushButton_plus, SIGNAL(clicked()), this, SLOT(culcClicked()));
+    connect(ui->pushButton_minus, SIGNAL(clicked()), this, SLOT(culcClicked()));
+    connect(ui->pushButton_mult, SIGNAL(clicked()), this, SLOT(culcClicked()));
+    connect(ui->pushButton_div, SIGNAL(clicked()), this, SLOT(culcClicked()));
+
+
+
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+QString MainWindow::calculate(const QString& op){
+    bool ok_1;
+    bool ok_2;
+    int num1 = ui->lineEdit_num1->text().toInt(&ok_1);
+    int num2 = ui->lineEdit_num2->text().toInt(&ok_2);
+    if(ok_1 && ok_2){
+        if(op == "+"){
+            return QString::number(num1 + num2);
+        }
+        if(op == "-"){
+            return QString::number(num1 - num2);
+        }
+
+        if(op == "*"){
+            return QString::number(num1 * num2);
+        }
+        if(op == "/"){
+            if( num2 == 0) return "Error";
+            return QString::number(num1 / num2);
+        }
+    }
+
+    return "Error";
+}
+
+
+
+void MainWindow::culcClicked(){
+    QPushButton  *button = (QPushButton *)sender(); //возвращает кнопку
+    ui->lineEdit_operator->setText(button->text());
+    ui->lineEdit_result->setText(calculate(button->text()));
+}
+
+
+
+
---
+++ mainwindow.h
@@ -0,0 +1,26 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QString>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+    QString calculate(const QString& op);
+private slots:
+    void culcClicked();
+
+};
+#endif // MAINWINDOW_H
